5ef36ffc9e9042df01c41970bef9c615d894fa7f
ojooss <53396131+ojooss@users.noreply.github.com>
2020-10-03 23:37:22 +0200

    added entity "station"

---
+++ src/Entity/Station.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\StationRepository")
+ */
+class Station
+{
+
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private ?int $id;
+
+    /**
+     * @ORM\Column(type="string", length=128)
+     */
+    private string $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private string $url;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private ?int $sequenceNr;
+
+    /**
+     * @var bool
+     * @Groups({"station.get","put","post"})
+     */
+    public bool $isBeingPlayed = false;
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     * @return $this
+     */
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getUrl(): ?string
+    {
+        return $this->url;
+    }
+
+    /**
+     * @param string $url
+     * @return $this
+     */
+    public function setUrl(string $url): self
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getSequenceNr(): ?int
+    {
+        return $this->sequenceNr;
+    }
+
+    /**
+     * @param int|null $sequenceNr
+     * @return $this
+     */
+    public function setSequenceNr(?int $sequenceNr): self
+    {
+        $this->sequenceNr = $sequenceNr;
+
+        return $this;
+    }
+
+
+}
---
+++ src/Repository/StationRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Station;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method Station|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Station|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Station[]    findAll()
+ * @method Station[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class StationRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Station::class);
+    }
+
+    // /**
+    //  * @return Pipe[] Returns an array of Pipe objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('p')
+            ->andWhere('p.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('p.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?Pipe
+    {
+        return $this->createQueryBuilder('p')
+            ->andWhere('p.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
