c9852130b3fa3262c2fc2a24399ce2964d43a9e0
ojooss <github@o-jooss.de>
2024-11-25 22:24:39 +0100

    fixed rector.php

--- rector.php
+++ rector.php
@@ -3,15 +3,15 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Core\ValueObject\PhpVersion;
 use Rector\Doctrine\Set\DoctrineSetList;
 use Rector\Php74\Rector\Closure\ClosureToArrowFunctionRector;
 use Rector\Set\ValueObject\LevelSetList;
-use Rector\Symfony\Set\SymfonyLevelSetList;
+use Rector\Symfony\Set\SymfonySetList;
+
 
 return static function (RectorConfig $rectorConfig): void {
 
-    $rectorConfig->phpVersion(PhpVersion::PHP_81);
+    $rectorConfig->phpVersion(\Rector\ValueObject\PhpVersion::PHP_84);
     $rectorConfig->disableParallel();
 
     $rectorConfig->paths([
@@ -21,7 +21,7 @@ return static function (RectorConfig $rectorConfig): void {
     // define sets of rules
     $rectorConfig->sets([
         LevelSetList::UP_TO_PHP_81,
-        SymfonyLevelSetList::UP_TO_SYMFONY_63,
+        SymfonySetList::SYMFONY_64,
         DoctrineSetList::DOCTRINE_BUNDLE_210,
         DoctrineSetList::DOCTRINE_ORM_25,
     ]);
