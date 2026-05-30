c1cb6d72c9e1ff4fbe10cc3d33fb565616aa309a
ojooss <github@o-jooss.de>
2026-05-30 17:26:08 +0200

    Update tooling config: PHPStan level 7, PHPUnit 11, Rector for PHP 8.5
    - phpstan: raise to level 7, exclude reference config, add known ignore
    - phpunit: migrate to PHPUnit 11 format (no listeners, source block)
    - rector: rewrite with new fluent API targeting PHP 8.5

--- rector.php
+++ rector.php
@@ -9,28 +9,17 @@ use Rector\Set\ValueObject\LevelSetList;
 use Rector\Symfony\Set\SymfonySetList;
 
 
-return static function (RectorConfig $rectorConfig): void {
-
-    $rectorConfig->phpVersion(\Rector\ValueObject\PhpVersion::PHP_84);
-    $rectorConfig->disableParallel();
-
-    $rectorConfig->paths([
-        __DIR__ . '/src' ,
-        __DIR__ . '/tests' ,
-    ]);
-
-    // define sets of rules
-    $rectorConfig->sets([
-        LevelSetList::UP_TO_PHP_84,
-        SymfonySetList::SYMFONY_64,
-        SymfonySetList::ANNOTATIONS_TO_ATTRIBUTES,
-        DoctrineSetList::DOCTRINE_BUNDLE_210,
-        DoctrineSetList::DOCTRINE_ORM_213,
-        DoctrineSetList::DOCTRINE_ORM_214,
-        DoctrineSetList::DOCTRINE_ORM_25,
-        DoctrineSetList::DOCTRINE_ORM_29,
-    ]);
-    $rectorConfig->skip([
-        ClosureToArrowFunctionRector::class,
-    ]);
-};
+return RectorConfig::configure()
+    ->withPhpVersion(\Rector\ValueObject\PhpVersion::PHP_85)
+    ->withoutParallel()
+    ->withPaths([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
+    ->withImportNames(removeUnusedImports: true)
+    ->withComposerBased(twig: true)
+    ->withAttributesSets(
+        symfony: true,
+        doctrine: true,
+        phpunit: true
+    );
